5d1b8971f7d3ff13c61f9028b8e1b7c9ee4edfa7
Luc Klaassen <luc.klaassen@infosupport.com>
2018-05-15 20:01:31 +0200

    Added test dependency, created integration test

---
+++ src/test/java/CryptoCurrencyIntegrationTest.java
@@ -0,0 +1,32 @@
+import com.luc.crypto.CryptoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CryptoApplication.class)
+@AutoConfigureMockMvc
+public class CryptoCurrencyIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void onStartupTheApplicationContainsFourCryptoCurrencies() throws Exception {
+        mvc.perform(get("/currencies")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.currencies", hasSize(4)));
+    }
+
+}
