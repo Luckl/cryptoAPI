58aba357bc18aaaed5a63ae682571320f3e88ca1
Luc Klaassen <luc.klaassen@infosupport.com>
2018-05-15 20:02:13 +0200

    moved integrationtest to package

--- src/test/java/com/luc/crypto/CryptoCurrencyIntegrationTest.java
+++ src/test/java/com/luc/crypto/CryptoCurrencyIntegrationTest.java
@@ -1,17 +1,18 @@
-import com.luc.crypto.CryptoApplication;
+package com.luc.crypto;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CryptoApplication.class)
